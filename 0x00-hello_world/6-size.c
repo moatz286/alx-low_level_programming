e85be74f0fd11e37872363cf8dbf6f6760fa9a4c
Mutaz Abd Alrhman <moatz286@gmail.com>
2022-10-14 07:55:18 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  * Description: 'print datatype'
  * Return: Always 0 (Success)
  */
- 
 int main(void) 
 {
 int intType;
@@ -14,12 +13,11 @@ float floatType;
 double doubleType;
 char charType;
 
-    
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-    
+
 return (0);
 }
