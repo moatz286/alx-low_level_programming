d084d677b1992c482efbaee0c2ee7f9c9146df2b
Mutaz Abd Alrhman <moatz286@gmail.com>
2022-10-13 17:42:54 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/* print string with printf */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
