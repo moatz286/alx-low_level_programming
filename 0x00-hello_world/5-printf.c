a88821c6fc9020ec74cc376684178c7900be500b
Mutaz Abd Alrhman <moatz286@gmail.com>
2022-10-13 18:04:58 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+
 /**
- * main - entry point
- * Description: print srting with printf
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
