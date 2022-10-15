8ac45f5fb9987fc79c21ff4c7faa0810f914ff0b
Mutaz Abd Alrhman <moatz286@gmail.com>
2022-10-14 18:18:31 -0700

    vars & conds

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0) {
-		printf("%n is positive\n");
+		printf("%n is positive\n", n);
 	}
 	else if (n < 0) {
-		printf("%n is negative\n");
+		printf("%n is negative\n", n);
 	}
 	else {
-		printf("%n is zero\n");
+		printf("%n is zero\n", n);
 	}
 	return (0);
 }
