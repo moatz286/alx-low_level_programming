0c7f84dc6aecaf470f03223932bab383ad153b8f
Mutaz <moatz286@gmail.com>
2025-10-14 22:06:10 +0200

    style fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,16 +8,16 @@
  */
 void print_array(int *array, size_t left, size_t right)
 {
-    size_t i;
+	size_t i;
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
 }
 
 /**
@@ -30,27 +30,27 @@ void print_array(int *array, size_t left, size_t right)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left, right, mid;
+	size_t left, right, mid;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    left = 0;
-    right = size - 1;
+	left = 0;
+	right = size - 1;
 
-    while (left <= right)
-    {
-        print_array(array, left, right);
+	while (left <= right)
+	{
+		print_array(array, left, right);
 
-        mid = (left + right) / 2;
+		mid = (left + right) / 2;
 
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
